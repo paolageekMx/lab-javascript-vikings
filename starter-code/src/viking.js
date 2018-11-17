5237c289f4490777a371d87ea68bc7827ac086e3
Repositorio Paola <paola@aureapaola.com>
2018-11-17 12:48:22 -0600

    Actualizacion

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,38 @@
-// Soldier
-function Soldier() {}
+
+function Soldier(healthArg, strength) {
+  
+  this.attack = function () {
+    return strength;
+  }
+
+  this.receiveDamage = function (damage) {
+    this.health -=  damage;
+    if (this.health > 0) {
+      return this.name + " has received DAMAGE points of damage";
+    } else {
+      this.name + " has died in act of combat"
+    }
+  }
+ 
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+ Soldier.call(this, attack);
+  return strength;
+  
+this.receiveDamage= function (damage) {
+this.health -= damage;
+if (this.health > 0) {
+  return this.name + " has received DAMAGE points of damage";
+} else {
+  this.name + " has died in act of combat"
+}
+}
+this.battleCry = function () {
+return ("Odin Owns You All")
+}
+}
 
 // Saxon
 function Saxon() {}
